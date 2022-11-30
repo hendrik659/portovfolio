d8ec045a63aaae182c49eced24a5d0342bfa5dcf
hendrik659 <109999021+hendrik659@users.noreply.github.com>
2022-11-30 09:55:45 +0800

    first commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
         Selamat Datang <br />
-        Di Fortofolio ku
+        Fortofolio ku
       </SectionTitle>
       <SectionText>
-        Portofolio adalah kumpulan hasil kerja atau karya seseorang yang di susun secara sistematis.
-        Artinya, portofolio harus di buat secara sistematis untuk memudahkan proses penilaian terhadap kompetensi seseorang.
+        Portofolio adalah kumpulan hasil kerja atau karya seseorang yang di
+        susun secara sistematis. Artinya, portofolio harus di buat secara
+        sistematis untuk memudahkan proses penilaian terhadap kompetensi
+        seseorang.
       </SectionText>
-      <Button onClick={() => window.location = 'https://google.com'} >Pelajari</Button>
+      <Button onClick={() => (window.location = "https://google.com")}>
+        Pelajari
+      </Button>
     </LeftSection>
   </Section>
 );
